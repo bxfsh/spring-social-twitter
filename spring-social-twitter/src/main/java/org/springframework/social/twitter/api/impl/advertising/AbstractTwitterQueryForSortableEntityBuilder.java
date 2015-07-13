031669a182293638eb269623f9f8cb163fb1b0e9
Hudson Mendes <hudsonmendes@gmail.com>
2015-07-13 16:29:49 -0700

    Fix for sorting (in abstract, so many requests might have been fixed)

--- spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/advertising/AbstractTwitterQueryForSortableEntityBuilder.java
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/advertising/AbstractTwitterQueryForSortableEntityBuilder.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,8 +34,8 @@ import org.springframework.util.MultiValueMap;
  * @param <TSort> the sort enumberation that varies for each entity being requested.
  */
 public abstract class AbstractTwitterQueryForSortableEntityBuilder<TBuilderInterface extends TwitterQueryForEntity<TBuilderInterface>, TSort>
-        extends AbstractTwitterQueryForEntityBuilder<TBuilderInterface>
-        implements TwitterQueryForSortableEntity<TBuilderInterface, TSort> {
+extends AbstractTwitterQueryForEntityBuilder<TBuilderInterface>
+implements TwitterQueryForSortableEntity<TBuilderInterface, TSort> {
 
     private TSort sort;
     private SortDirection direction;
@@ -67,7 +67,7 @@ public abstract class AbstractTwitterQueryForSortableEntityBuilder<TBuilderInter
 
         final String sort = this.makeSort();
         if (sort != null)
-            appendParameter(map, "sort", sort);
+            appendParameter(map, "sort_by", sort);
 
         return map;
     }
