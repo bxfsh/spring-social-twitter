c386e5194c5180c3ca1d436ed1c247fe6b3c5081
Hudson Mendes <hudsonmendes@gmail.com>
2015-07-13 16:49:42 -0700

    Another fix for null sort_direction

--- spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/advertising/AbstractTwitterQueryForSortableEntityBuilder.java
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/api/impl/advertising/AbstractTwitterQueryForSortableEntityBuilder.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014 the original author or authors.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,8 +34,8 @@ import org.springframework.util.MultiValueMap;
  * @param <TSort> the sort enumberation that varies for each entity being requested.
  */
 public abstract class AbstractTwitterQueryForSortableEntityBuilder<TBuilderInterface extends TwitterQueryForEntity<TBuilderInterface>, TSort>
-extends AbstractTwitterQueryForEntityBuilder<TBuilderInterface>
-implements TwitterQueryForSortableEntity<TBuilderInterface, TSort> {
+        extends AbstractTwitterQueryForEntityBuilder<TBuilderInterface>
+        implements TwitterQueryForSortableEntity<TBuilderInterface, TSort> {
 
     private TSort sort;
     private SortDirection direction;
@@ -77,8 +77,8 @@ implements TwitterQueryForSortableEntity<TBuilderInterface, TSort> {
             return null;
 
         if (this.direction != null)
-            return this.sort + "-" + this.direction.name();
+            return this.sort + "-" + this.direction;
 
-        return this.sort.toString();
+        return this.sort.toString() + "-" + SortDirection.asc;
     }
 }
